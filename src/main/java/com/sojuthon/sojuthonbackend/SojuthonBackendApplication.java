076ac2819abf418dc36779082898731bb6449572
kdomo <dongho5088@naver.com>
2022-12-03 13:18:42 +0900

    init

---
+++ src/main/java/com/sojuthon/sojuthonbackend/SojuthonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sojuthon.sojuthonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SojuthonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SojuthonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sojuthon/sojuthonbackend/SojuthonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sojuthon.sojuthonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SojuthonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
