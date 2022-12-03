ce174bae39e8cf347e79e2607784f8389633f520
kdomo <dongho5088@naver.com>
2022-12-03 14:09:40 +0900

    chore : AWS s3 setting

--- src/main/java/com/sojuthon/sojuthonbackend/SojuthonBackendApplication.java
+++ src/main/java/com/sojuthon/sojuthonbackend/SojuthonBackendApplication.java
@@ -2,12 +2,20 @@ package com.sojuthon.sojuthonbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SojuthonBackendApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application-dev.yml,"
+			+ "classpath:aws.yml";
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(SojuthonBackendApplication.class, args);
+		new SpringApplicationBuilder(SojuthonBackendApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+//		SpringApplication.run(SojuthonBackendApplication.class, args);
 	}
 
 }
